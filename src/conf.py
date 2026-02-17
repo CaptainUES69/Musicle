f04eb6a69188e2d364f32b02ebda19d934b6cd85
CaptainUES69 <69493833+CaptainUES69@users.noreply.github.com>
2026-02-17 06:50:05 +0500

    refactor: немного подправил определение query параметров

--- src/conf.py
+++ src/conf.py
@@ -1,17 +1,26 @@
-from logging import INFO, Formatter, getLogger
+from logging import INFO, Formatter, getLogger, Logger
 from logging.handlers import RotatingFileHandler
 
-# Настройки логгирования
-logger = getLogger("App")
-logger.level = INFO  # Уровень логирования
 
-handler = RotatingFileHandler(
-    filename = "app.log", maxBytes = (5 * 1024 * 1024), backupCount = 5, encoding = "utf-8"
-)
 
-formatter = Formatter(
-    "%(asctime)s %(levelname)s -- %(funcName)s(%(lineno)d) - %(message)s"
-)
-handler.setFormatter(formatter)
+class Logging:
+    logger: Logger
 
-logger.addHandler(handler)
+    def __init__(
+            self,
+            filename: str,
+            loggerName: str     
+        ):
+        self.logger = getLogger(loggerName)
+        self.logger.level = INFO
+
+        handler = RotatingFileHandler(
+            filename = filename, 
+            maxBytes = (5 * 1024 * 1024), 
+            backupCount = 5, 
+            encoding = 'utf-8'
+        )
+        formatter = Formatter("%(asctime)s %(levelname)s -- %(funcName)s(%(lineno)d) - %(message)s")
+        handler.setFormatter(formatter)
+        self.logger.addHandler(handler)
+    
--- src/main.py
+++ src/main.py
@@ -3,29 +3,51 @@ from fastapi.responses import StreamingResponse
 
 from .audio import AudioFile
 from .main_logic import MainLogic
+from .conf import Logging
+from typing import List, Dict, Annotated
 
+from pydantic import BaseModel
+
+logger = Logging(filename='webserver.log',loggerName='webserver')
 audio = AudioFile()
 app = FastAPI()
 logic = MainLogic()
 
 
-@app.get('/hard')
-async def hard(artist_name: str, rounds: int = 5, snippet_length_ms: int = 10000):
-    return await logic.metadata_async(artist_name, rounds, snippet_length_ms)
+
+
+@app.get('/hard/{artist_name}')
+async def hard(
+    artist_name: str, 
+    rounds: int = Query(5), 
+    length_ms: int = Query(10000)
+) -> List[Dict]:
+    return await logic.metadata_async(artist_name, rounds, length_ms)
     
 
-@app.get('/medium')
-async def medium(artist_name: str, rounds: int = 5, snippet_length_ms: int = 15000):
-    return await logic.metadata_async(artist_name, rounds, snippet_length_ms)
+@app.get('/medium/{artist_name}')
+async def medium(
+    artist_name: str, 
+    rounds: int = 5, 
+    length_ms: int = Query(15000)
+) -> List[Dict]:
+    return await logic.metadata_async(artist_name, rounds, length_ms)
 
 
-@app.get('/easy')
-async def easy(artist_name: str, rounds: int = 5, snippet_length_ms: int = 20000):
-    return await logic.metadata_async(artist_name, rounds, snippet_length_ms)
+@app.get('/easy/{artist_name}')
+async def easy(
+    artist_name: str, 
+    rounds: int = 5, 
+    length_ms: int = Query(20000)
+) -> List[Dict]:
+    return await logic.metadata_async(artist_name, rounds, length_ms)
 
 
 @app.get('/snippet/{track_id}')
-async def snippet(track_id: str | int, length_ms: int = Query(15000)):
+async def snippet(
+    track_id: str | int,
+    length_ms: int = Query(15000)
+) -> StreamingResponse:
     file = await logic.trim_track_async(track_id, length_ms)
 
     return StreamingResponse(
